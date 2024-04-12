0e17b32de7b3b1a2e47d6643dc414e7d6dc87d99
Jonathan Bryant <jonathan-bryant@outlook.com>
2024-04-13 09:48:42 +1000

    WIP optimizations + emote size functionality

--- src/emote-side-panel/index.js
+++ src/emote-side-panel/index.js
@@ -4,7 +4,7 @@ import STYLE_URL from './styles.scss';
 class EmoteSidePanel extends Addon {
 
 	getContainer() {
-		return document.querySelector(".chat-list--default,.chat-list--other");
+		return document.querySelector('.chat-list--default,.chat-list--other');
 	}
 
 	getPanel() {
@@ -17,11 +17,11 @@ class EmoteSidePanel extends Addon {
 		});
 
 		const container = this.getContainer();
-		container.style.setProperty("position", "relative");
+		container.style.setProperty('position', 'relative');
 		container.append(newPanel);
 
 		if (this.settings.get('emote_side_panel.override_width')) {
-			newPanel.style.width = this.settings.get('emote_side_panel.width_in_px') + 'px';
+			newPanel.style.width = `${this.settings.get('emote_side_panel.width_in_px')}px`;
 		}
 
 		return newPanel;
@@ -30,21 +30,86 @@ class EmoteSidePanel extends Addon {
 	updatePadding() {
 		// Had some problems changing the chat width, so for now i'll leave it fixed
 		const padding = (this.emotes.length > 0) ? 50 : 50;
-		this.getContainer().querySelector(".simplebar-content").style.setProperty('padding-right', padding + 'px');
+		this.getContainer().querySelector('.simplebar-content').style.setProperty('padding-right', `${padding}px`);
+	}
+
+	updateHighlight() {
+		if (this.settings.get('emote_side_panel.highlight_max_count')) {
+			let maxCountEmote = null;
+			let maxCount = 0;
+
+			for (const emote of this.emotes) {
+				const emoteImage = emote.element.querySelector('img');
+				if (emoteImage) {
+					// Reset styles for all emotes
+					emoteImage.style.maxHeight = 'unset';
+					emoteImage.style.height = 'unset';
+					emoteImage.style.maxwidth = 'unset';
+					emoteImage.style.minWidth = 'unset';
+
+					// Adjust the src to use the smallest version of the emote from the srcset based on 1X, 2X, and 4X
+					const srcset = emoteImage.getAttribute('srcset');
+					if (srcset) {
+						const srcs = srcset.split(',');
+						const src = srcs[0].split(' ')[0];
+						if (emoteImage.src !== src) {
+							emoteImage.src = src;
+						}
+					}
+
+					// Find the emote with the maximum count
+					if (emote.instances.length > maxCount && this.settings.get('emote_side_panel.sort_order') === 'none') {
+						maxCount = emote.instances.length;
+						maxCountEmote = emote;
+					}
+				}
+			}
+
+			if (this.settings.get('emote_side_panel.sort_order') === 'ascending') {
+				maxCountEmote = this.emotes[this.emotes.length - 1];
+			} else if (this.settings.get('emote_side_panel.sort_order') === 'descending') {
+				maxCountEmote = this.emotes[0];
+			}
+
+			// Apply styles to the emote with the maximum count
+			if (maxCountEmote) {
+				const sizeIncrease = this.settings.get('emote_side_panel.size_increase');
+				const emoteImage = maxCountEmote.element.querySelector('img');
+				if (emoteImage) {
+					emoteImage.style.maxwidth = `${sizeIncrease}px`;
+					emoteImage.style.minWidth = `${sizeIncrease}px`;
+					emoteImage.style.maxHeight = 'auto';
+					emoteImage.style.height = `${sizeIncrease}px`;
+
+					// Adjust the src to use the largest version of the emote from the srcset based on 1X, 2X, and 4X
+					const srcset = emoteImage.getAttribute('srcset');
+					if (srcset) {
+						const srcs = srcset.split(',');
+						const src = srcs[srcs.length - 1].split(' ')[0];
+						if (emoteImage.src !== src) {
+							emoteImage.src = src;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	updateCount(emote) {
 		const len = emote.instances.length;
-		emote.element.querySelector("span").innerHTML = ((len == 1) ? "" : "x" + len);
+		emote.element.querySelector('span').innerHTML = ((len == 1) ? '' : `x${len}`);
 	}
 
 	updateElement(emote) {
 		const panel = this.getPanel();
 		const el = emote.element;
-		if (panel.lastChild !== el) {
+		if (panel.lastChild !== el && this.settings.get('emote_side_panel.sort_order') === 'none') {
 			// Send emote to the end
 			panel.removeChild(el);
 			panel.appendChild(el);
+			el.classList.add('animate')
+			this.setRemoveAnimation(el);
+		} else {
 			el.classList.add('animate')
 			this.setRemoveAnimation(el);
 		}
@@ -77,7 +142,7 @@ class EmoteSidePanel extends Addon {
 
 	sortEmotes() {
 		const panel = this.getPanel();
-		if (this.settings.get('emote_side_panel.sort_by_count') && this.emotes.length > 1) {
+		if (this.emotes.length > 1) {
 			const sortOrder = this.settings.get('emote_side_panel.sort_order');
 			if (sortOrder === 'ascending') {
 				this.emotes.sort((a, b) => a.instances.length - b.instances.length);
@@ -92,11 +157,12 @@ class EmoteSidePanel extends Addon {
 				}
 
 				// Add emotes back to the panel in the sorted order
-				for (let emote of this.emotes) {
+				for (const emote of this.emotes) {
 					panel.appendChild(emote.element);
 				}
 			}
 		}
+		this.updateHighlight();
 	}
 
 	setUpdatePanel() {
@@ -104,15 +170,15 @@ class EmoteSidePanel extends Addon {
 	}
 
 	createEmoteElement(emote) {
-		return (<div class='animate'>
-			<span class='mult'></span>
+		return (<div class="animate">
+			<span class="mult"></span>
 			{this.chat.renderTokens([emote])}
 		</div>)
 	}
 
 	clearEmotes() {
 		this.emotes = [];
-		this.getPanel().innerHTML = "";
+		this.getPanel().innerHTML = '';
 	}
 
 	setRemoveAnimation(el) {
@@ -123,8 +189,6 @@ class EmoteSidePanel extends Addon {
 		// Avoid handling the message twice
 		if (msg.esp_handled) return tokens;
 		msg.esp_handled = true;
-
-		let removeMessage = false;
 
 		let emoteOnly = true;
 		for (const token of tokens) {
@@ -154,7 +218,7 @@ class EmoteSidePanel extends Addon {
 					const el = this.createEmoteElement(token, 1);
 					this.getPanel().appendChild(el);
 					this.setRemoveAnimation(el);
-					this.emotes.push({ text: text, element: el, firstTime: instance.timestamp, instances: [instance] });
+					this.emotes.push({ text, element: el, firstTime: instance.timestamp, instances: [instance] });
 				}
 			}
 		}
@@ -175,7 +239,7 @@ class EmoteSidePanel extends Addon {
 		this.settings.add('emote_side_panel.capture_all', {
 			default: false,
 			ui: {
-				path: 'Add-Ons > Emote Side Panel',
+				path: 'Add-Ons > Emote Side Panel >> Behaviour',
 				title: 'Capture all',
 				description: 'Capture emotes from all messages, even those that are not emote only',
 				component: 'setting-check-box',
@@ -185,7 +249,7 @@ class EmoteSidePanel extends Addon {
 		this.settings.add('emote_side_panel.sort_order', {
 			default: 'none',
 			ui: {
-				path: 'Add-Ons > Emote Side Panel',
+				path: 'Add-Ons > Emote Side Panel >> Appearance',
 				title: 'Sort Order',
 				description: 'Sort emotes by count',
 				component: 'setting-select-box',
@@ -200,7 +264,7 @@ class EmoteSidePanel extends Addon {
 		this.settings.add('emote_side_panel.keep_messages', {
 			default: false,
 			ui: {
-				path: 'Add-Ons > Emote Side Panel',
+				path: 'Add-Ons > Emote Side Panel >> Behaviour',
 				title: 'Keep messages',
 				description: 'Capture emotes but do not remove emote only messages',
 				component: 'setting-check-box',
@@ -210,18 +274,38 @@ class EmoteSidePanel extends Addon {
 		this.settings.add('emote_side_panel.timeout', {
 			default: 15,
 			ui: {
-				path: 'Add-Ons > Emote Side Panel',
+				path: 'Add-Ons > Emote Side Panel >> Behaviour',
 				title: 'Timeout',
 				description: 'Time in seconds that the emote is visible on the side panel',
 				component: 'setting-text-box',
 			},
-			changed: val => this.timeout = parseInt(val) == 0 ? 30 : parseInt(val)
+			changed: val => this.timeout = parseInt(val, 10) == 0 ? 30 : parseInt(val, 10)
+		});
+
+		this.settings.add('emote_side_panel.highlight_max_count', {
+			default: false,
+			ui: {
+				path: 'Add-Ons > Emote Side Panel >> Appearance',
+				title: 'Highlight Max Count',
+				description: 'Highlight the emote with the highest count',
+				component: 'setting-check-box',
+			},
+		});
+
+		this.settings.add('emote_side_panel.size_increase', {
+			default: 32,
+			ui: {
+				path: 'Add-Ons > Emote Side Panel >> Appearance',
+				title: 'Size Increase',
+				description: 'Set the increase in size for the highlighted emote in pixels',
+				component: 'setting-text-box',
+			},
 		});
 
 		this.settings.add('emote_side_panel.override_width', {
 			default: false,
 			ui: {
-				path: 'Add-Ons > Emote Side Panel',
+				path: 'Add-Ons > Emote Side Panel >> Appearance',
 				title: 'Override Panel Width',
 				description: 'Enable to set a custom width for the emote panel',
 				component: 'setting-check-box',
@@ -231,7 +315,7 @@ class EmoteSidePanel extends Addon {
 		this.settings.add('emote_side_panel.width_in_px', {
 			default: 32,
 			ui: {
-				path: 'Add-Ons > Emote Side Panel',
+				path: 'Add-Ons > Emote Side Panel >> Appearance',
 				title: 'Panel Width in Pixels',
 				description: 'Set the width of the emote panel in pixels',
 				component: 'setting-text-box',
@@ -241,7 +325,7 @@ class EmoteSidePanel extends Addon {
 		this.emotes = [];
 		this.updateTimer = null;
 		this.style_link = null;
-		this.timeout = parseInt(this.settings.get('emote_side_panel.timeout'));
+		this.timeout = parseInt(this.settings.get('emote_side_panel.timeout'), 10);
 
 		const outerThis = this;
 		this.messageFilter = {
